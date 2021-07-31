86ce2bcec363d0762a62d855d3e15c7c6278b6eb
Shashank Bhat <shashank.bhat2525@gmail.com>
2021-08-01 01:13:16 +0530

    styling changes

--- pages/index.js
+++ pages/index.js
@@ -28,18 +28,6 @@ const Home = () =>  {
                   <button>I'm Feeling Lucky</button>
                 </Link>
               </div>
-              <div className={styles.lang_options}>
-                 Google offered in:
-                 <a href="/">हिन्दी</a>
-                 <a href="/">বাংলা</a>
-                 <a href="/">తెలుగు</a>
-                 <a href="/">मराठी</a>
-                 <a href="/">தமிழ்</a>
-                 <a href="/">ગુજરાતી</a>
-                 <a href="/">ಕನ್ನಡ</a>
-                 <a href="/">മലയാളം</a>
-                 <a href="/">ਪੰਜਾਬੀ</a>
-              </div>
             </form>
           </div>
         </main>
