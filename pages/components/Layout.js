509ff91725d5d2b8bd935b81dea323a03bec1059
Shashank Bhat <shashank.bhat2525@gmail.com>
2021-07-31 22:16:11 +0530

    initial commit

---
+++ pages/components/Layout.js
@@ -0,0 +1,96 @@
+import Head from 'next/head'
+import AppsIcon from '@material-ui/icons/Apps';
+import styles from '../../styles/Layout.module.css'
+import Link from 'next/link'
+
+const Layout = ({children}) => {
+    return(
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet"/>
+        <title>Google | Assignment</title>
+      </Head>
+      <header>
+        <nav  className={styles.g_nav}>
+          <ul>
+            <li>
+              <Link href='/'>
+                <a>
+                  Gmail
+                </a>
+              </Link>
+            </li> 
+            <li>
+              <Link href='/'>
+                <a>
+                  Images
+                </a>
+              </Link>
+            </li> 
+          </ul>
+          <div className={styles.icons}>
+              <div  className={styles.menu}>
+                <AppsIcon />
+              </div> 
+              <div className={styles.avatar}>
+                  <p>
+                    MD
+                  </p>
+              </div> 
+          </div>
+        </nav>
+      </header>
+      {children}
+      <footer className={styles.footer}>
+          <div>
+             <p>India</p> 
+          </div>
+          <div className={styles.footer_links}>
+            <ul >
+              <li>
+                <a href="/">
+                  About
+                </a>
+              </li>
+              <li>
+                <a href="/">
+                  Advertising
+                </a>
+              </li>
+              <li>
+                <a href="/">
+                  Business
+                </a>
+              </li>
+              <li>
+                <a href="/">
+                  How Search Works
+                </a>
+              </li>
+            </ul>
+            <ul>
+            <li>
+                <a href="/">
+                  Privacy
+                </a>
+              </li>
+              <li>
+                <a href="/">
+                  Terms
+                </a>
+              </li>
+              <li>
+                <a href="/">
+                  Settings
+                </a>
+              </li>
+            </ul>
+          </div>
+        </footer>
+    </>
+    )
+}
+
+export default Layout;
--- pages/index.js
+++ pages/index.js
@@ -1,49 +1,11 @@
-import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import Link from 'next/link'
-import AppsIcon from '@material-ui/icons/Apps';
 import Search from '@material-ui/icons/Search'
 import { Mic } from '@material-ui/icons';
-
+import Link from 'next/link'
 
 const Home = () =>  {
   return (
     <>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet"/>
-      </Head>
-      <header>
-        <nav  className={styles.g_nav}>
-          <ul>
-            <li>
-              <Link href='/'>
-                <a>
-                  Gmail
-                </a>
-              </Link>
-            </li> 
-            <li>
-              <Link href='/'>
-                <a>
-                  Images
-                </a>
-              </Link>
-            </li> 
-          </ul>
-          <div className={styles.icons}>
-              <div  className={styles.menu}>
-                <AppsIcon />
-              </div> 
-              <div className={styles.avatar}>
-                  <p>
-                    J
-                  </p>
-              </div> 
-          </div>
-        </nav>
-      </header>
       <main className={styles.g_main}>
           <div>
             <img src='/google.png'/>
@@ -51,11 +13,11 @@ const Home = () =>  {
           <div className={styles.search_section}>
             <form className={styles.g_form}>
               <div  className={styles.g_search}>
-                <div className={styles.icons}>
+                <div>
                   <Search className={styles.search} />
                 </div>
-                <input type="text" name='q' />
-                <div className={styles.icons}>
+                <input type="text" />
+                <div>
                   <Mic className={styles.mic}/>
                 </div>
               </div>
@@ -80,52 +42,6 @@ const Home = () =>  {
             </form>
           </div>
         </main>
-        <footer className={styles.footer}>
-          <div>
-             <p>India</p> 
-          </div>
-          <div className={styles.footer_links}>
-            <ul >
-              <li>
-                <a href="/">
-                  About
-                </a>
-              </li>
-              <li>
-                <a href="/">
-                  Advertising
-                </a>
-              </li>
-              <li>
-                <a href="/">
-                  Business
-                </a>
-              </li>
-              <li>
-                <a href="/">
-                  How Search Works
-                </a>
-              </li>
-            </ul>
-            <ul>
-            <li>
-                <a href="/">
-                  Privacy
-                </a>
-              </li>
-              <li>
-                <a href="/">
-                  Terms
-                </a>
-              </li>
-              <li>
-                <a href="/">
-                  Settings
-                </a>
-              </li>
-            </ul>
-          </div>
-        </footer>
     </>
   )
 }
