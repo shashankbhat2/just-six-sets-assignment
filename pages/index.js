9feccf15567cc78b0da887c8b72be46348b6af5b
Shashank Bhat <shashank.bhat2525@gmail.com>
2021-07-31 22:29:12 +0530

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import styles from '../styles/Home.module.css'
-import Search from '@material-ui/icons/Search'
+import {Search} from '@material-ui/icons/'
 import { Mic } from '@material-ui/icons/';
 import Link from 'next/link'
 
